5a7067a6dcf1897ddb8b04077e43ef17bf248c9e
JavierNT1 <163213095+JavierNT1@users.noreply.github.com>
2024-04-25 18:13:07 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -40,9 +40,9 @@ int is_equal(void* key1, void* key2){
 
 long resolverColisiones(HashMap* map, int posInicial){
   long pos = posInicial;
-  long intentos = 0;
+  long intentos = 1;
 
-  while(map->buckets[pos] != NULL){
+  while(map->buckets[pos] != NULL && map->buckets[pos]->key != NULL){
     pos = (posInicial + intentos * intentos) % map->size;
     intentos++;
     if(intentos >= map->size)
