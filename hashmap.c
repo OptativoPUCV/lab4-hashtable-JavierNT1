23b985817caf8248caaf31ed280113a099887582
JavierNT1 <163213095+JavierNT1@users.noreply.github.com>
2024-04-25 23:16:25 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -102,7 +102,19 @@ void eraseMap(HashMap * map,  char * key) {
 }
 
 Pair * searchMap(HashMap * map,  char * key) {     
+  long pos = hash(key, map->capacity);
+  long posInicial = pos;
+  
+  while(map->buckets[pos] != NULL){
+    if(is_equal(map->buckets[pos]->key, key) == 1){
+      map->current = pos;
+      return map->buckets[pos]; 
+    }
+    pos = (pos + 1) % map->capacity;
 
+    if(pos == posInicial)
+      break;
+  }
   return NULL;
 }
 
